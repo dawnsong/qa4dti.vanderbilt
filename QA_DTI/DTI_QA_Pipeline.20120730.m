900e9cb25a7214141b363c0c059c47d5f6224952
dawnsong <dawnwei.song@gmail.com>
2013-11-12 00:26:22 -0500

    fix a typo

--- QA_DTI/DTI_QA_Pipeline.20120730.m
+++ QA_DTI/DTI_QA_Pipeline.20120730.m
@@ -163,9 +163,9 @@ name_Mask=sprintf('%s%sMask',tmp,filesep); name_Mask2=sprintf('%s%sMask2',tmp,fi
 %%%%eval(cmmd)
 %%%%cmmd=sprintf('!LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH  bet %s %s -f 0.4 -g 0 -m',name_bo_ref,name_Mask2);%for statistical analysis 
 %%%%eval(cmmd) 
-cmmd=sprintf('bet %s %s -f 0.2 -g 0 -m',name_bo_ref,name_Mask); %for use with camino
+cmmd=sprintf('!bet %s %s -f 0.2 -g 0 -m',name_bo_ref,name_Mask); %for use with camino
 eval(cmmd)
-cmmd=sprintf('bet %s %s -f 0.4 -g 0 -m',name_bo_ref,name_Mask2);%for statistical analysis 
+cmmd=sprintf('!bet %s %s -f 0.4 -g 0 -m',name_bo_ref,name_Mask2);%for statistical analysis 
 eval(cmmd) 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
