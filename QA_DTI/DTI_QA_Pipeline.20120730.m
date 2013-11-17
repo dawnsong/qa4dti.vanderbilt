a23a1361cb761dfeee0bcc345bdf82ebdd58acac
dawnsong <dawnwei.song@gmail.com>
2013-11-16 23:12:50 -0500

    minor change

--- QA_DTI/DTI_QA_Pipeline.20120730.m
+++ QA_DTI/DTI_QA_Pipeline.20120730.m
@@ -213,7 +213,7 @@ tem_name=sprintf('%s_mask.nii',name_Mask);
 temp=load_nii(tem_name); %temp=temp.img;
 %[centerX centerY centerZ]=com(temp); 
 %dawnsong revise
-[centerX centerY centerZ]=size(temp.img)/2; 
+[centerX centerY centerZ]=temp.hdr.dime.dim(2:4)/2; 
 centerX=round(centerX); centerY=round(centerY); centerZ=round(centerZ); %find better center of image
 clear temp tem_name
 pack
